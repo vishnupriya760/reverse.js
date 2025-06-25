9dacd3322a91ab29fe37522ee4bd328d94dc1119
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-25 16:05:54 +0530

    Add files via upload

---
+++ reverse.js
@@ -0,0 +1,70 @@
+let a=20
+while (a>=0){
+console.log(a);
+a--
+}
+
+let num=30
+while(num>=3){
+    console.log(num);
+
+    num-=3
+}
+
+let nu=50
+while(nu>=5){
+    console.log(nu);
+
+    nu-=5
+}
+n=15
+while(n>=1){
+    console.log(n**2);
+     n-=2
+}
+
+let y=10
+let k=5
+
+while(y>=1){
+    console.log(`${y}*${k}=${k*y}`)
+    y--
+
+  
+    }
+    let ci=15
+    while(ci>=1){
+        if(ci%3==0 && ci%5==0){
+            console.log('fizzbuzz')
+            
+        }
+        else if(ci%5==0){
+            console.log('buzz')
+
+            
+        }
+        else if(ci%3==0){
+            console.log('fizz')
+            
+            
+        }
+        else{
+            console.log(ci)
+            
+        }
+
+        ci--
+        
+    }
+    sm=0;
+    let i=15
+    while(i>=1){
+        sm=i*i;
+        i-=2;
+        console.log('sum of odd numbers',sm)
+        
+        
+
+    }
+  
+   
